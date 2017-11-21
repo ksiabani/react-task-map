703e1643981f2122d2a874270f44f0887c12cf11
Kostas Siabanis <kostas.siabanis@gmail.com>
2017-11-21 21:33:30 +0200

    Add header and cards components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './Map';
 import MapRoute from './RouteMap';
+import Header from './modules/components/Header';
+import Tasks from './modules/components/Tasks';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <MapRoute/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <MapRoute/>
+                <Tasks/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/modules/components/Tasks.js
+++ src/modules/components/Tasks.js
@@ -1,3 +1,27 @@
-/**
- * Created by ksiabani on 21/11/17.
- */
+import React, {Component} from 'react';
+
+class Tasks extends Component {
+    render() {
+        return (
+            <div className="cards">
+                <div className="card">
+                    <div className="card__img">
+                        <img src="https://d1xqbpwl1wh09p.cloudfront.net/avatars/60/fa7c/c030229b/60fa7cc030229b12a9b17f7071e143e5.jpg" />
+                    </div>
+                    <div className="card__content">
+                        <h2 className="card__content__title">iPhone SE</h2>
+                        <h3 className="card__content__route">Oslo → Oslo</h3>
+                        <h4 className="card__content__size">
+                            <span>XL</span>Fits in a big trailer
+                        </h4>
+                    </div>
+                    <div className="card__price">€ 140</div>
+                </div>
+                {/*   <div class="cards__push-left"><</div> */}
+                <div className="cards__push-right">&gt;</div>
+            </div>
+        )
+    }
+}
+
+export default Tasks;
